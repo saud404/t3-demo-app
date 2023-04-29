ac6b79145d96a580100da4871185bab27a9e8e9b
saud rehman <saud.ur.rehman1@gmail.com>
2023-04-29 09:08:30 +0500

    Build Ready 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ import Image from "next/image";
 import Navbar from "~/components/NavBar";
 // import Footer from "~/components/Footer";
 // import Quote from "~/components/Quote";
-import Hero from "~/assets/images/home.jpg";
+// import Hero from "../assets/images/home.jpg";
 import Features from "~/components/Features";
 // const CreatePostWizard = () => {
 //   const { user } = useUser();
@@ -117,7 +117,11 @@ const Home: NextPage = () => {
       <main className="">
         <Navbar />
         <div className=" relative ">
-          <Image src={Hero} alt="Home" className="mx-auto h-[80vh] md:w-full" />
+          <Image
+            src="../assets/images/home.jpg"
+            alt="Home"
+            className="mx-auto h-[80vh] md:w-full"
+          />
 
           <div className=" md:left-94 absolute z-10 mx-auto mt-20 w-full rounded-2xl bg-white p-4  md:right-60 md:top-40 md:w-[70%]">
             <div className="  sm:pl-[0px] md:pl-[300px]">
