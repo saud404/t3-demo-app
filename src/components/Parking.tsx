19796973f416ffb98ec6301d63787c67cbaa99ae
saud rehman <saud.ur.rehman1@gmail.com>
2023-05-10 17:01:36 +0500

    This is done 7

--- src/components/Parking.tsx
+++ src/components/Parking.tsx
@@ -1,43 +1,42 @@
-import React, { useState } from "react";
-import { DatePicker } from "react-responsive-datepicker";
-import "react-responsive-datepicker/dist/index.css";
-import { TimePicker } from "react-ios-time-picker";
-const Parking = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [value, setValue] = useState("10:00");
+// import React, { useState } from "react";
+// import "react-responsive-datepicker/dist/index.css";
+// import { TimePicker } from "react-ios-time-picker";
+// const Parking = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+//   const [value, setValue] = useState("10:00");
 
-  const onChange = (timeValue) => {
-    setValue(timeValue);
-  };
-  //   if (typeof window !== 'undefined') {
-  //     React.render(<MainWrapper />, document.getElementById("root"));
-  // }
-  return (
-    <>
-      <div>
-        <button
-          onClick={() => {
-            setIsOpen(true);
-          }}
-          //   onChange={onChange}
-        >
-          open
-        </button>
-        <DatePicker
-          isOpen={isOpen}
-          onClose={() => setIsOpen(false)}
-          defaultValue={new Date(2022, 8, 8)}
-          minDate={new Date(2022, 10, 10)}
-          maxDate={new Date(2023, 0, 10)}
-          headerFormat="DD, MM dd"
-        />
-        {/* <TimePicker onChange={onChange} value={value} /> */}
-      </div>
-      {/* <div>
-        <TimePicker onChange={onChange} value={value} use12Hours />
-      </div> */}
-    </>
-  );
-};
+//   const onChange = (timeValue) => {
+//     setValue(timeValue);
+//   };
+//   //   if (typeof window !== 'undefined') {
+//   //     React.render(<MainWrapper />, document.getElementById("root"));
+//   // }
+//   return (
+//     <>
+//       <div>
+//         <button
+//           onClick={() => {
+//             setIsOpen(true);
+//           }}
+//           //   onChange={onChange}
+//         >
+//           open
+//         </button>
+//         <DatePicker
+//           isOpen={isOpen}
+//           onClose={() => setIsOpen(false)}
+//           defaultValue={new Date(2022, 8, 8)}
+//           minDate={new Date(2022, 10, 10)}
+//           maxDate={new Date(2023, 0, 10)}
+//           headerFormat="DD, MM dd"
+//         />
+//         {/* <TimePicker onChange={onChange} value={value} /> */}
+//       </div>
+//       {/* <div>
+//         <TimePicker onChange={onChange} value={value} use12Hours />
+//       </div> */}
+//     </>
+//   );
+// };
 
-export default Parking;
+// export default Parking;
